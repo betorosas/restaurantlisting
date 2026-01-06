7c88a0d7a3eedb06fe54e36034ac6d5078f6f000
betorosas <figure_0.9@hotmail.com>
2026-01-05 20:21:33 -0600

    Update RestaurantService.java

--- src/main/java/com/codeddecode/restaurantlisting/service/RestaurantService.java
+++ src/main/java/com/codeddecode/restaurantlisting/service/RestaurantService.java
@@ -22,21 +22,24 @@ public class RestaurantService {
 
 
     public List<RestaurantDTO> findAllRestaurants() {
-        List<Restaurant> restaurants = restaurantRepo.findAll();
-        List<RestaurantDTO> restaurantDTOList = restaurants.stream().map(restaurant -> RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(restaurant)).collect(Collectors.toList());
-        return restaurantDTOList;
+//        List<Restaurant> restaurants = restaurantRepo.findAll();
+//        List<RestaurantDTO> restaurantDTOList = restaurants.stream().map(restaurant -> RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(restaurant)).collect(Collectors.toList());
+//        return restaurantDTOList;
+    	return null;
     }
 
     public RestaurantDTO addRestaurantInDB(RestaurantDTO restaurantDTO) {
-        Restaurant savedRestaurant =  restaurantRepo.save(RestaurantMapper.INSTANCE.mapRestaurantDTOToRestaurant(restaurantDTO));
-        return RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(savedRestaurant);
+//        Restaurant savedRestaurant =  restaurantRepo.save(RestaurantMapper.INSTANCE.mapRestaurantDTOToRestaurant(restaurantDTO));
+//        return RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(savedRestaurant);
+    	return null;
     }
 
     public ResponseEntity<RestaurantDTO> fetchRestaurantById(Integer id) {
-        Optional<Restaurant> restaurant =  restaurantRepo.findById(id);
-        if(restaurant.isPresent()){
-            return new ResponseEntity<>(RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(restaurant.get()), HttpStatus.OK);
-        }
-        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+//        Optional<Restaurant> restaurant =  restaurantRepo.findById(id);
+//        if(restaurant.isPresent()){
+//            return new ResponseEntity<>(RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(restaurant.get()), HttpStatus.OK);
+//        }
+//        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    	return null;
     }
 }
